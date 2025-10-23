22a70800d041d469bdfe13ebdd09eaa1950c6ced
obijuan <obijuan.cube@gmail.com>
2025-10-23 21:29:01 +0200

    update comments

--- main.py
+++ main.py
@@ -175,35 +175,35 @@ def test_addi():
 
 def test_slli():
     inst_slli = [
-        0x00009013,
-        0x00119113,
-        0x00329213,
-        0x010a1513,
-        0x01ff9f13,
+        0x00009013,  # slli x0, x1, 0
+        0x00119113,  # slli x2, x3, 1
+        0x00329213,  # slli x4, x5, 3
+        0x010a1513,  # slli x10, x20, 16
+        0x01ff9f13,  # slli x30, x31, 31
     ]
     print_inst(inst_slli, 'SLLI')
 
 
 def test_slti():
     insts = [
-        0x0000a013,
-        0x0011a113,
-        0x0032a213,
-        0x7ff62593,
-        0x80072693,
-        0xfff82793,
+        0x0000a013,  # slti x0, x1, 0
+        0x0011a113,  # slti x2, x3, 1
+        0x0032a213,  # slti x4, x5, 3
+        0x7ff62593,  # slti x11, x12, 2047
+        0x80072693,  # slti x13, x14, -2048
+        0xfff82793,  # slti x15, x16, -1
     ]
     print_inst(insts, 'SLTI')
 
 
 def test_sltiu():
     insts = [
-        0x0073b313,
-        0x00f4b413,
-        0x0ff5b513,
-        0xff66b613,
-        0xf9c7b713,
-        0xfff8b813,
+        0x0073b313,  # sltiu x6, x7, 7
+        0x00f4b413,  # sltiu x8, x9, 15
+        0x0ff5b513,  # sltiu x10, x11, 255
+        0xff66b613,  # sltiu x12, x13, -10
+        0xf9c7b713,  # sltiu x14, x15, -100
+        0xfff8b813,  # sltiu x16, x17, -1
     ]
     print_inst(insts, 'SLTIU')
 
