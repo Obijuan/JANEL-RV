8dc81020806f15dfcf660e80bc098774a12c2c34
obijuan <obijuan.cube@gmail.com>
2025-10-24 00:17:23 +0200

    update instruction documentation

--- InstrRV.py
+++ InstrRV.py
@@ -8,6 +8,7 @@ https://msyksphinz-self.github.io/riscv-isadoc/html/rvi.html
 
  TIPO I: Instrucciones aritméticas y de carga
  * addi, slli, slti, sltiu, xori, srli, srai, ori, andi
+ * lb, lh, lw, ld, lbu, lhu, lwu
 
  3 3 2 2 2 2 2 2 2 2 2 2 1 1 1 1 1 1 1 1 1 1
 │1 0 9 8 7 6 5 4 3 2 1 0│9 8 7 6 5│4 3 2│1 0 9 8 7  │6 5 4 3 2 1 0│
