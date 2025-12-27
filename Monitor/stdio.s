b6139dd84b2602748d43102e5a95ed97e549a335
obijuan <obijuan.cube@gmail.com>
2025-12-27 11:22:02 +0100

    add sprint_bin1

--- Monitor/stdio.s
+++ Monitor/stdio.s
@@ -3,8 +3,8 @@
 	#-- Longitud maxima de las cadenas
 	.eqv MAX 255
 
-	#-- Numero maximo de unarios a imprimir
-	.eqv MAX_UNARY 5
+	#-- Mascaras para BITs
+	.eqv BIT0 0x01
 
 	#-- Servicios del sistema operativo del RARs
 	.include "rars_so.s"
@@ -23,11 +23,67 @@ dst:	.space MAX
 	#-- Prueba de SPRINT_UNARY
 	jal sprint_test2
 
+	#-- Prueba de SPRINT_BIN1
+	jal sprint_test3
+
+
+	#-- TODO: 
+	#-- sprint_bin2
+	#-- sprint_bin3
+	#-- sprint_bin4
+	#-- sprint_bin8
+	#-- sprint_bin16
+	#-- sprint_bin32
+
 	#-- Terminar
 	PRINT_CHARI('\n')
 	EXIT
 	
+#--------------------------------------
+#-- Pruebas para SPRINT_BIN1
+#-- Imprimiendo numeros binarios de 1 bit
+#--------------------------------------
+	.data
+test3_msg1: .string "Bit: "
 
+	.eqv MAX_BIN1 2
+
+	.text
+sprint_test3:
+	#-- Crear pila
+	addi sp, sp, -16
+	sw ra, 12(sp)
+	sw s0, 8(sp)
+
+	PRINT_STRINGI("\n* TEST 3:\n")
+    
+	li s0, 0  #-- Numero a imprimir
+
+  sprint_test3_next:
+
+	#-- Imprimir mensaje
+	la a0, dst
+	la a1, test3_msg1
+	jal sprint
+
+	mv a1, s0
+	jal sprint_bin1
+
+	PRINT_STRINGL(dst)
+	PRINT_CHARI('\n')
+
+	#-- Siguiente numero
+	addi s0, s0, 1
+
+	#-- Hemos alcanzado el maximo?
+	li t1, MAX_BIN1
+    blt s0, t1, sprint_test3_next
+
+	#-- Restaurar pila
+	lw ra, 12(sp)
+	lw s0, 8(sp)
+	addi sp, sp, 16	
+	ret
 
 #--------------------------------------
 #-- Pruebas para SPRINT_UNARY
@@ -35,6 +91,9 @@ dst:	.space MAX
 #--------------------------------------
 	.data
 test2_msg1: .string "Unario: "
+
+	#-- Numero maximo de unarios a imprimir
+	.eqv MAX_UNARY 5
 
 	.text
 sprint_test2:
@@ -64,16 +123,13 @@ sprint_test2_next:
 	PRINT_CHARI('\n')
 
 	li t0, MAX_UNARY
-    beq s0, t0, fin
+    beq s0, t0, sprint_test2_fin
 
 	#-- Incrementar numero unario
 	addi s0, s0, 1
 	j sprint_test2_next
 
-fin:	
-
-
-
+sprint_test2_fin:	
 
 	#-- Restaurar pila
 	lw ra, 12(sp)
@@ -125,6 +181,36 @@ sprint_test1:
 	ret
 
 #--------------------------------------------------
+#-- SPRINT_BIN1(dst, n)
+#-- Imprimir un numero binario de 1 bit
+#--
+#--  ENTRADAS:
+#--   - a0 (dst): Puntero a cadena destino
+#--   - a1 (n): Numero a imprimir
+#--  SALIDA:
+#--   - a0: Puntero al final de la cadena destino
+#--   - a1: (Opcional) Nº de bits impresos
+#--------------------------------------------------
+sprint_bin1:
+	#-- Quedarse con el bit 0
+	andi a1, a1, BIT0
+
+	#-- Convertir a caracter: '0' o '1'
+	addi a1, a1, '0'
+
+	#-- Almacenar caracter en cadena destino
+	sb a1, 0(a0)
+
+	#-- Incrementar puntero de cadena destino
+	addi a0, a0, 1
+
+	#-- Cadena terminada
+	sb zero, 0(a0)
+
+	li a1, 1  #-- Un bit impreso
+	ret
+
+#--------------------------------------------------
 # SPRINT_UNARY(dst, n, mark)
 #-- Imprimir un numero en unario
 #--
@@ -140,7 +226,7 @@ sprint_unary:
 	#-- Contador de marcas
 	li t0, 0
 
-sprint_unary_bucle:
+  sprint_unary_bucle:
 
 	#-- Si t0==0, terminar. No hay marcas que imprimir
 	beq a1, zero, sprint_unary_end
@@ -157,7 +243,7 @@ sprint_unary_bucle:
 	#-- Repetir
 	j sprint_unary_bucle
 
-sprint_unary_end:
+  sprint_unary_end:
 	sb zero, 0(a0)  #-- Cadena terminada
 
 	#-- a1: Contador de caracteres
@@ -182,7 +268,7 @@ sprint:
 	li t0, 0
 	
 	#-- Bucle principal
-sprint_bucle:
+  sprint_bucle:
 	#-- Leer caracter de cadena fuente
 	lb t1, 0(a1)
 	
@@ -207,7 +293,7 @@ sprint_bucle:
 	j sprint_bucle
 	
 	
-sprint_end:
+  sprint_end:
 	#-- Hemos terminado de copiar
 	#-- a0 apunta al final de la cadena destino
 	
