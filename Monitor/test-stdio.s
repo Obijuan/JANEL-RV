9fb7234d42debc18668dc8df14278a5a7961c946
obijuan <obijuan.cube@gmail.com>
2026-01-06 20:05:12 +0100

    stdio: add unittests for sprint_bcd_digit()

--- Monitor/test-stdio.s
+++ Monitor/test-stdio.s
@@ -979,21 +979,35 @@ test_bin1:
 unittest_sprint_bcd_digit:
 
 	.data
-test14_tittle:  .string "----- SPRINT_BCD_DIGIT()-----\n"
-test14_name:    .string "> TEST 14...."
-test14_result1: .string "0"
-test15_name:    .string "> TEST 15...."
-test15_result1: .string "1"
-test16_name:    .string "> TEST 16...."
-test16_result1: .string "01234567"
-
-
+ test14_tittle:  .string "----- SPRINT_BCD_DIGIT()-----\n"
+ test14_name:    .string "> TEST 14...."
+ test14_result1: .string "0"
+ test15_name:    .string "> TEST 15...."
+ test15_result2: .string "1"
+ test16_name:    .string "> TEST 16...."
+ test16_result3: .string "01234567"
+ test17_name:    .string "> TEST 17...."
+ test17_result4: .string "0123456789"
+ test18_name:    .string "> TEST 18...."
+ test18_result5: .string "0123456789ABCDEF"
+ test19_name:    .string "> TEST 19...."
+ test19_cad:     .string "Bit: "
+ test19_result6: .string "Bit: 0"
+ test20_name:    .string "> TEST 20...."
+ test20_cad:     .string "Bin: "
+ test20_result6: .string "Bin: 10"
+ test21_name:    .string "> TEST 21...."
+ test21_cad1:    .string "Digito: "
+ test21_cad2:    .string "-----\n"
+ test21_result7: .string "Digito: 9-----\n"
+ 
 	.text
 	STACK16
 
 	#--- Imprimir titulo
 	la a0, test14_tittle
 	jal puts
+
 
 	#--------  14. Imprimir Bit 0
 	la a0, test14_name
@@ -1009,6 +1023,7 @@ test16_result1: .string "01234567"
 	la a1, test14_result1
 	jal assert_str_equal
 
+
 	#--------  15. Imprimir Bit 1
 	la a0, test15_name
 	jal puts
@@ -1020,8 +1035,9 @@ test16_result1: .string "01234567"
 
 	#-- assert buffer == '1'
 	la a0, buffer
-	la a1, test15_result1
+	la a1, test15_result2
 	jal assert_str_equal
+
 
 	#--------  16. Imprimir todos los digitos octales
 	la a0, test16_name
@@ -1047,7 +1063,128 @@ test16_result1: .string "01234567"
 
 	#-- assert buffer == '01234567'
 	la a0, buffer
-	la a1, test16_result1
+	la a1, test16_result3
+	jal assert_str_equal
+
+
+	#--------  17. Imprimir todos los digitos decimales
+	la a0, test17_name
+	jal puts
+
+	#-- Contador de digitos
+	li s0, 0
+
+	#-- Direccion del buffer
+	la a0, buffer
+
+ next17:
+	#-- sprint_bcd_digit(buffer, dig)
+	mv a1, s0
+	jal sprint_bcd_digit
+
+	#-- Incrementar contador
+	addi s0, s0, 1
+
+	#-- Mientras digito actual sea menor a 10, repetir
+	li t0, 10
+	blt s0, t0, next17
+
+	#-- assert buffer == '0123456789'
+	la a0, buffer
+	la a1, test17_result4
+	jal assert_str_equal
+
+
+	#--------  18. Imprimir todos los digitos hexadecimales
+	la a0, test18_name
+	jal puts
+
+	#-- Contador de digitos
+	li s0, 0
+
+	#-- Direccion del buffer
+	la a0, buffer
+
+ next18:
+	#-- sprint_bcd_digit(buffer, dig)
+	mv a1, s0
+	jal sprint_bcd_digit
+
+	#-- Incrementar contador
+	addi s0, s0, 1
+
+	#-- Mientras digito actual sea menor a 16, repetir
+	li t0, 16
+	blt s0, t0, next18
+
+	#-- assert buffer == '0123456789'
+	la a0, buffer
+	la a1, test18_result5
+	jal assert_str_equal
+
+
+	#--------- 19. Imprimir Cadena+Digito BCD
+	la a0, test19_name
+	jal puts
+
+	#-- sprint(buffer, "Bit: ")
+	la a0, buffer
+	la a1, test19_cad
+	jal sprint
+
+	#-- sprint_bcd_digit(buffer, 0)
+	li a1, 0
+	jal sprint_bcd_digit
+
+	#-- assert buffer == '0'
+	la a0, buffer
+	la a1, test19_result6
+	jal assert_str_equal
+
+
+	#--------- 20. Imprimir Cadena+Digito+Digito
+	la a0, test20_name
+	jal puts
+
+	#-- sprint(buffer, "Bin: ")
+	la a0, buffer
+	la a1, test20_cad
+	jal sprint
+
+	#-- sprint_bcd_digit(buffer, 1)
+	li a1, 1
+	jal sprint_bcd_digit
+
+	#-- sprint_bcd_digit(buffer, 0)
+	li a1, 0
+	jal sprint_bcd_digit
+
+	#-- assert buffer == 'Bin: 10'
+	la a0, buffer
+	la a1, test20_result6
+	jal assert_str_equal
+
+
+	#---------- 21. Imprimir Cadena + Digito + Cadena
+	la a0, test21_name
+	jal puts
+
+	#-- sprint(buffer, "Digito: ")
+	la a0, buffer
+	la a1, test21_cad1
+	jal sprint
+
+	#-- sprint_bcd_digit(buffer, 9)
+	li a1, 9
+	jal sprint_bcd_digit
+
+	#-- sprint(buffer, "-----\n")
+	la a1, test21_cad2
+	jal sprint
+
+	#-- assert_str_equal(buffer, "Digito: 9-----\n")
+	la a0, buffer
+	la a1, test21_result7
 	jal assert_str_equal
 
 	UNSTACK16
